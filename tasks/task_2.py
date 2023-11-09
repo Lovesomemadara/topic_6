ff2f41e00fb607d8f37cb05a30fea43cbd81cc86
Lovesomemadara <borovkun@gmail.com>
2023-11-09 13:57:52 +0500

    Не все задания выполнены, доделаю в другом городе

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,1 +1,12 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num: int = int(input("Введите целое число: "))
+
+'''if num > 0:
+    print(1)
+elif num == 0:
+    print(0)
+else:
+    print(-1)'''
+
+result: int = 1 if num > 0 else 0 if num == 0 else -1
+
+print(result)
--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,1 +1,9 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+year: int = int(input("Введите год: "))
+month: int = int(input("Введите номер месяца: "))
+
+if ((year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+        and ((month < 8 and month % 2 != 0)
+             or (month >= 8 and month % 2 == 0))):
+    print("Да")
+else:
+    print("Нет")
