ce58949ccafdd0c09b0e7e2d37e8db008a442d9b
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-11-10 11:26:33 +0000

    Проверка ДЗ | 6. Условные операторы
    - Правильно: №0, №1, №2, №4, №5
    - Неправильно:
    - Улучшены: №0, №4, №5,
    - На доработку: №3,

--- tasks/task_4.py
+++ tasks/task_4.py
@@ -4,9 +4,20 @@ num_3: int = int(input("Введите третье целое число: "))
 
 if num_1 > num_2 and num_1 > num_3:
     print(f"Наибольшее число: {num_1}")
-elif num_2 > num_1 and num_2 > num_3:
+elif num_2 > num_3:
     print(f"Наибольшее число: {num_2}")
 elif num_1 == num_2 == num_3:
     print(f"Все числа равны")
 else:
     print(f"Наибольшее число: {num_3}")
+
+# ----------№2----------
+
+if num_1 > num_2 and num_1 > num_3:
+    print(f"Наибольшее число: {num_1}")
+elif num_2 > num_3:
+    print(f"Наибольшее число: {num_2}")
+elif num_3 > num_2:
+    print(f"Наибольшее число: {num_3}")
+else:
+    print(f"Все числа равны")
--- tasks/task_5.py
+++ tasks/task_5.py
@@ -2,7 +2,7 @@ num: int = int(input("Введите целое число: "))
 
 if num % 2 == 0:
     print(f"Число {num} является четным")
-elif num % 2 != 0 and num * 3 > 20:
+elif num * 3 > 20:
     print(f"Результат умножения {num} на 3 больше 20")
 else:
     print(f"Число {num} не соответствует условиям")
